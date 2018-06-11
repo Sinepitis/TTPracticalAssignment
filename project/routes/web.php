09b29f6e6090854ffd400281058de05bf05f92e2
kaaposts125 <rkokins7@gmail.com>
2018-06-11 05:09:43 +0300

    Added create and delete functions

--- project/routes/web.php
+++ project/routes/web.php
@@ -1,4 +1,5 @@
 <?php
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -9,26 +10,52 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
 Route::get('/', function () {
     return view('home');
 })->middleware('roles:0');;
+
 Route::get('/about', function () {
     return view('about');
 })->middleware('roles:0');
+
 Route::get('/contact', function () {
     return view('contact');
 })->middleware('roles:0');
+
 Route::get('/register', function () {
     return view('register');
 })->middleware('roles:0');
+
 Route::get('/games', 'GamesController@index')->middleware('roles:0');
+
+
 Route::get('/teams', 'TeamController@index')->middleware('roles:0');
+
 Route::get('/players', 'ParticipantController@index')->middleware('roles:0');
+
 Route::get('/leagues', 'LeagueController@index')->middleware('roles:0');
+
 Route::get('/tournaments', 'TournamentController@index')->middleware('roles:0');
+
 Route::get('/admin','AdminController@GetUsers')->middleware('roles:2');
+
 Route::post('/admin/assign-roles',['uses'=>'AdminController@PostAdminAssignRoles','as' => 'admin.assign'])->middleware('roles:2');
+
 Route::post('/register/submit','RegisterController@submit')->middleware('roles:0');
+
 Auth::routes();
+
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/leagues/Add','LeagueController@create')->middleware('roles:2');
+
+Route::get('/players/Add','ParticipantController@create')->middleware('roles:2');
+
+Route::get('/tournaments/Add','TournamentController@create')->middleware('roles:2');
+
+Route::get('/teams/Add','TeamController@create')->middleware('roles:2');
+
+Route::get('/games/Add', 'GamesController@create')->middleware('roles:2');
+Route::post('/games/Delete', 'GamesController@delete')->middleware('roles:0');
 Route::post('/contact/submit', 'MessagesController@submit');
